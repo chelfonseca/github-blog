158d4571cc3744849ff588beb2875cd75ac25208
chelfonseca <michelbeserradafonseca@gmail.com>
2023-11-13 15:50:28 -0300

    Search component

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,10 +1,12 @@
 import { Profile } from "./components/Profile";
+import { Search } from "./components/Search";
 import { BlogContainer } from "./styles";
 
 export function Blog(){
     return (
         <BlogContainer>
             <Profile/>
+            <Search/>
 
         </BlogContainer>
     )
